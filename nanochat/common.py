ee7c0d9042a6c871437beec268f3a91f9c8b9708
kyleqihua <kyleqihuahuang@gmail.com>
2025-12-01 14:36:16 +0800

    feat: add get_dist_info() for distributed training support

--- nanochat/common.py
+++ nanochat/common.py
@@ -3,3 +3,9 @@ import os
 def get_base_dir():
     home_dir = os.path.expanduser("~")
     return os.path.join(home_dir, ".cache", "nanochat")
+
+def get_dist_info():
+    ddp = int(os.environ.get('RANK', -1)) != -1
+    if ddp:
+        return True, int(os.environ['RANK']), int(os.environ['LOCAL_RANK']), int(os.environ['WORLD_SIZE'])
+    return False, 0, 0, 1
